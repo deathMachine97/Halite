6dffd5c169bbfdfcf3fd0e1949a489588ce38709
deathMachine97 <mukhtarabeshov@gmail.com>
2019-06-18 20:45:21 +0600

    Enter

--- MyBot.py
+++ MyBot.py
@@ -19,67 +19,34 @@ import logging
 
 """ <<<Game Begin>>> """
 
-# This game object contains the initial game state.
 game = hlt.Game()
-# At this point "game" variable is populated with initial map data.
-# This is a good place to do computationally expensive start-up pre-processing.
-# As soon as you call "ready" function below, the 2 second per turn timer will start.
 game.ready("MyPythonBot")
-
-# Now that your bot is initialized, save a message to yourself in the log file with some important information.
-#     Here, you log here your id, which you can always fetch from the game object by using my_id.
 logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
-
-
 def create_ship():
 	command_queue.append(me.shipyard.spawn())
-
-
 directions = [Direction.North, Direction.South, Direction.East, Direction.West]
 
+now_doing = Direction.North
 """ <<<Game Loop>>> """
 while True:
-	# This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
-	#     running update_frame().
 	game.update_frame()
-	# You extract player metadata and the updated map metadata here for convenience.
 	me = game.me
 	game_map = game.game_map
-
-	# A command queue holds all the commands you will run this turn. You build this list up and submit it at the
-	#     end of the turn.
 	command_queue = []
-
+	if me.halite_amount >= 4500:
+		now_doing = Direction.North
 	for ship in me.get_ships():
 		new_direction = random.choice(directions)
-		now_doing = Direction.North
 		control = False 
-		# choices = ship.position.get_surrounding_cardinals()
-		# logging.info(Direction.North)
-		# For each of your ships, move randomly if the ship is on a low halite location or the ship is full.
-		#     Else, collect halite.
 		if game_map[ship.position].halite_amount < constants.MAX_HALITE / 100 or ship.is_full:
+			if ship.is_full:
+				now_doing = Direction.South
 			command_queue.append(
 				ship.move(now_doing)
 			)
 		else:
 			command_queue.append(ship.stay_still())
 
-		if ship.is_full and control==False:
-			control=True
-			command_queue.clear()
-			command_queue.append(
-				ship.move(Direction.South)
-			)
-
 	if game.turn_number == 1:
 		create_ship()
-
-	# If the game is in the first 200 turns and you have enough halite, spawn a ship.
-	# Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
-	# if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
-	# create_ship()
-	# command_queue.append(me.shipyard.spawn())
-
-	# Send your moves back to the game environment, ending this turn.
 	game.end_turn(command_queue)
