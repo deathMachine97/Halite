c0c963bec2546d85aa5c2fad131aed34bc63fdab
deathMachine97 <mukhtarabeshov@gmail.com>
2019-06-19 09:14:20 +0600

    Нахождение оптимольного путя

--- MyBot.py
+++ MyBot.py
@@ -28,25 +28,53 @@ directions = [Direction.North, Direction.South, Direction.East, Direction.West]
 
 now_doing = Direction.North
 """ <<<Game Loop>>> """
+
+def function_name(kShip,kBase):
+	# если корабль находится под базой
+	iHeight_map = 32
+	bIs_ship_on_top = True if kBase[1]-kShip[1]>0 else False
+	if bIs_ship_on_top  == True:
+		# если корабль находится над базой
+		iShipvverh = iHeight_map - kBase[1] + kShip[1]
+		iShipvniz = kBase[1] - kShip[1] 
+	else:
+		# если корабль находится под базой
+		iH_ship= iHeight_map - kShip[1]
+		iTop_base = iHeight_map + kShip[0] - kBase[1]
+		iShipvniz = iH_ship + iTop_base
+		iShipvverh = iHeight_map - kBase[1] - iH_ship
+	# print(f"вверх {iShipvverh} Вниз {iShipvniz}")
+	if iShipvverh<iShipvniz:
+		return Direction.North
+	else:
+		return Direction.South
+
+
 while True:
 	game.update_frame()
 	me = game.me
 	game_map = game.game_map
 	command_queue = []
-	if me.halite_amount >= 4500:
-		now_doing = Direction.North
 	for ship in me.get_ships():
-		new_direction = random.choice(directions)
-		control = False 
+		kShip = (ship.position.x,ship.position.y)
+		kBase = (me.shipyard.position.x,me.shipyard.position.y)
+		# Если энергии 0, то идет на север
+		if ship.halite_amount == 0:
+			now_doing = Direction.North
+		# Если клеткада достаточно энергии или корабль полный
 		if game_map[ship.position].halite_amount < constants.MAX_HALITE / 100 or ship.is_full:
+			# Если корабль полный
+			logging.info(f"1 {now_doing}")
 			if ship.is_full:
-				now_doing = Direction.South
+				now_doing = function_name(kShip,kBase)
+				# logging.info(f"2 {now_doing}")
+
 			command_queue.append(
 				ship.move(now_doing)
 			)
 		else:
+			# Оставаться на месте
 			command_queue.append(ship.stay_still())
-
 	if game.turn_number == 1:
 		create_ship()
 	game.end_turn(command_queue)
