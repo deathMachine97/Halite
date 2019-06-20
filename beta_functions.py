d7d8c47e7619d67eb5fbbc5f2ed01ea2d0d0f2ab
DeathMachine97 <mukhtarabeshov@gmail.com>
2019-06-20 20:56:19 +0600

    Нахождение оптимального путя корабля в любой точке

--- beta_functions.py
+++ beta_functions.py
@@ -1,20 +1,21 @@
 """
 
 	1	2	3	4	5	6	7	8	9	10	11
-1												1
-2												2
-3												3
-4												4
-5												5
-6												6
-7												7
-8				*								8
-9				0						/		9
-10												10
-11												11
+1				|							*	1
+2				|								2
+3				|								3
+4				|								4
+5				|								5
+6				|								6
+7				|								7
+8				|								8
+9	-	-	-	0	-	-	-	-	-	-	-	9
+10				|								10
+11				|								11
 	1	2	3	4	5	6	7	8	9	10	11	
 
 """
+import random
 def find_best_direction_to_home(tBase_position,tShip_position,sType):
 	iMap_width = iMap_height = 11
 	iBase_coordinate = tBase_position[1] if sType == "vertically" else tBase_position[0]
@@ -30,10 +31,36 @@ def find_best_direction_to_home(tBase_position,tShip_position,sType):
 		iIndicator2_turn_count = iMap_height - iH_base+iH_ship
 	sIndicator_1_name = "top" if sType == "vertically" else "left"
 	sIndicator_2_name = "bottom" if sType == "vertically" else "right"
-	return sIndicator_1_name if iIndicator1_turn_count<iIndicator2_turn_count else sIndicator_2_name
+	optimal_turn_name = sIndicator_1_name if iIndicator1_turn_count<iIndicator2_turn_count else sIndicator_2_name
+	optimal_turn_count = iIndicator1_turn_count if iIndicator1_turn_count<iIndicator2_turn_count else iIndicator2_turn_count
+	return (optimal_turn_count,optimal_turn_name)
 
-sType = "hor"
-tBase_position = (4,9)
-tShip_position = (10,9)
-result = find_best_direction_to_home(tBase_position,tShip_position,sType)
-print(result)
+def find_best_direction_to_base_axis(tBase_position,tShip_position):
+	dChoice = {}
+	dChoice["horizontal"] = find_best_direction_to_home(tBase_position,tShip_position,"horizontal")
+	dChoice["vertically"] = find_best_direction_to_home(tBase_position,tShip_position,"vertically")
+	if dChoice["horizontal"][0] == dChoice["vertically"][0]:
+		return dChoice[random.choice(["horizontal","vertically"])]
+	else:
+		return dChoice["horizontal"] if dChoice["horizontal"][0]< dChoice["vertically"][0] else dChoice["vertically"]
+
+def go_home():
+	tBase_position = (4,9)
+	tShip_position = (4,9)
+	if tBase_position[0] == tShip_position[0] and tBase_position[1] == tShip_position[1]:
+		result = (0,"stay_still")
+	elif tBase_position[0] != tShip_position[0] and tBase_position[1] != tShip_position[1]:
+		print(f"2 условие")
+		result = find_best_direction_to_base_axis(tBase_position,tShip_position)
+	else:
+		print(f"3 условие")
+		sType = "vertically" if tBase_position[0] == tShip_position[0] else "horizontal"
+		result = find_best_direction_to_home(tBase_position,tShip_position,sType)
+
+	print(result)
+
+
+go_home()
+# result = find_best_direction_to_home(tBase_position,tShip_position,sType)
+# result = find_best_direction_to_home(tBase_position,tShip_position,sType)
+# print(result)
