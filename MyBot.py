562261dccf195af1fb6fd5987e87d3fa515be758
DeathMachine97 <mukhtarabeshov@gmail.com>
2019-06-20 22:51:56 +0600

    Записал планы, удалил ненужные коммы

--- MyBot.py
+++ MyBot.py
@@ -1,28 +1,13 @@
 #!/usr/bin/env python3
 # Python 3.6
-# Import the Halite SDK, which will let you interact with the game.
 import hlt
-# This library contains constant values.
 from hlt import constants
-# This library contains direction metadata to better interface with the game.
 from hlt.positionals import Direction
-# This library allows you to generate random numbers.
 import random
-# Logging allows you to save messages for yourself. This is required because the regular STDOUT
-# (print statements) are reserved for the engine-bot communication.
 import logging
 """ <<<Game Begin>>> """
-# This game object contains the initial game state.
 game = hlt.Game()
-# At this point "game" variable is populated with initial map data.
-# This is a good place to do computationally expensive start-up pre-processing.
-# As soon as you call "ready" function below, the 2 second per turn timer will start.
 game.ready("MyPythonBot")
-# Now that your bot is initialized, save a message to yourself in the log file with some important information.
-#     Here, you log here your id, which you can always fetch from the game object by using my_id.
-# logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
-
-
 def create_ship():
 	command_queue.append(me.shipyard.spawn())
 
